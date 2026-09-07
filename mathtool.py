6c2b20c94356104320d4137d2c8e84d396285a7f
Бесчастный Вадим <tyvi566788@gmail.com>
2026-09-07 22:30:07 +0300

    1.0

--- mathtool.py
+++ mathtool.py
@@ -6,33 +6,41 @@ def main():
     args = len(sys.argv) - 1
     
     if args == 0 or sys.argv[1] == "--help":
-        print("mathtool — решение уравнений вида A*x^2 + B*x + C = 0"
-
-"Использование:"
-    "python mathtool.py                         вывод справки"
-    "python mathtool.py --help                  вывод справки"
-    "python mathtool.py solve                   ввод коэффициентов с клавиатуры"
-    "python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами"
-
-"Коэффициенты A, B, C — целые числа, по модулю не превышающие 10000.")
+        print(
+"mathtool — решение уравнений вида A*x^2 + B*x + C = 0                            "
+"Использование:                                                                   "
+"python mathtool.py                         вывод справки                         "
+"python mathtool.py --help                  вывод справки                         "
+"python mathtool.py solve                   ввод коэффициентов с клавиатуры       "
+"python mathtool.py solve -a 1 -b -3 -c 2   решение с заданными коэффициентами    "
+"Коэффициенты A, B, C — целые числа, по модулю не превышающие 10000.              "
+)
         sys.exit(0)
     elif sys.argv[1] != "solve":
         print("Неизвестная команда")
         sys.exit(1)
-    elif sys.argv[1] == "solve" and len(sys.argv) != 2:
-        a = int(input("Введите A: "))
-        b = int(input("Введите B: "))
-        c = int(input("Введите C: "))
-    elif sys.argv[7] and len(sys.argv):
+    elif sys.argv[1] == "solve" and len(sys.argv) == 2:
+        a1 = str(input("Введите A: "))
+        b1 = str(input("Введите B: "))
+        c1 = str(input("Введите C: "))
+    elif len(sys.argv) == 8:
         if sys.argv[2] != "-a" and sys.argv[4] != "-b" and sys.argv[6] != "-c":
             print("Неизвестный параметр")
             sys.exit(1)
-        a = int(args(3))
-        b = int(args[5])
-        c = int(args[7])
+        a1=sys.argv[3]
+        b1=sys.argv[5]
+        c1=sys.argv[7]
     else:
         print("Неверный набор параметров") 
         sys.exit(1) 
+
+    try:
+        a = int(a1)
+        b = int(b1)
+        c = int(c1)
+    except ValueError:
+           print("ОШИБКА: коэффициент не является целым числом")
+            
 
     D = (b**2) - (4*a*c)
 
@@ -46,19 +54,25 @@ def main():
                 x2 = (-b - D**(1/2))/(2*a)
                 print (f"x1 = {x1:.3f}")
                 print (f"x2 = {x2:.3f}")
+                print ("Уравнение квадратное")
     elif D < 0 and a !=0 and b != 0:
-                print ("no")
+                print ("Действительных корней нет")
     elif D == 0 and a !=0 and b != 0:
                 x = -b / (2*a)
                 print(f"x = {x:.3f}")
+                print("Уравнение квадратное")
     elif a == 0:
                 x = -c / b
-                print (f"x = {x:.3f}")   
+                print (f"x = {x:.3f}") 
+                print("Уравнение линейное")  
     elif b == 0 and c > 0:
-                print ("шыш")
+                print ("Действительных корней нет")
     elif b == 0 and c <= 0:
                 x = (-c/a)**(1/2)
                 print (f"x = {x:.3f}")
+                print("Уравнение квадратное")
+    elif b == 0 and a == 0:
+           print("ОШИБКА: это не уравнение, неизвестное отсутствует")
     sys.exit(0)
     
 
