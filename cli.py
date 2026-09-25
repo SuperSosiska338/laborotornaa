8a81789b28b574910d7225e55dd002b0b05e22eb
Бесчастный Вадим <tyvi566788@gmail.com>
2026-09-25 10:49:23 +0300

    2.12

--- cli.py
+++ cli.py
@@ -2,12 +2,12 @@ import argparse
 
 def commandos():
     parser = argparse.ArgumentParser(prog="mathtool", description=
-"mathtool — позволяет выполнять расчёты над уравнениями и числовыми последовательностями"
+"mathtool — позволяет выполнять расчёты над уравнениями и числовыми последовательностями",
  allow_abbrev=False)
 
     subparsers = parser.add_subparsers(dest="command")
 
-    solveC = subparsers.add_parser("solve", help="Решение уравнения с заданными коэффициентами", allow_abbrev=False)
+    solveC = subparsers.add_parser("solve", help="Решение уравнения", allow_abbrev=False)
 
     solveC.add_argument("-a", type=int, help="Коэффициент A:\nцелые, по модулю не более 10 000")
     solveC.add_argument("-b", type=int, help="Коэффициент B:\nцелые, по модулю не более 10 000")
@@ -21,16 +21,16 @@ def commandos():
     seriesC = subparsers.add_parser("series", help="Выводит формулу ряда, вычисляет и выводит его сумму",
      allow_abbrev=False)
 
-    seriesC.add_argument("--func", required=True, help="Задаёт какой ряд суммировать: sqplus или third")
+    seriesC.add_argument("--func", required=True, help="Задаёт какой ряд суммировать")
 
     FUNCargument = seriesC.add_mutually_exclusive_group(required=True)
     FUNCargument.add_argument("--terms", type=int, help="Задаёт сколько слагаемых сложить:\nостановка по количеству. Целое от 1 до 10 000")
     FUNCargument.add_argument("--eps", type=float, help="Задаёт до какой величины слагремого считать:\nостановка по точности. Конечное число: больше нуля и не грубее 0,0001")
 
 
-    integrateC = subparsers.add_parser("integrate", help="Численное интегрирование функции", allow_abbrev=False)
+    integrateC = subparsers.add_parser("integrate", help="численное интегрирование функции", allow_abbrev=False)
 
-    integrateC.add_argument("--func", required=True, help="имя интегрируемой функции: ratio или root")
+    integrateC.add_argument("--func", required=True, help="имя интегрируемой функции")
     integrateC.add_argument("--from",type=float, dest="a", required=True, help="нижний предел")
     integrateC.add_argument("--to", type=float, dest="b", required=True, help="верхний предел")
     integrateC.add_argument("--steps", type=int, required=True, help="количество шагов")
